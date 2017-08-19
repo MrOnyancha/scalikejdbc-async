806a826bdb7283f29214cbc57aedbfb736785542
Onyancha Chrispinus <onyanchachrispinus@gmail.com>
2017-08-19 10:32:06 +0300

    PlayPlugin.scala
    For play 2.6, Please modify this source code to support the deprecated play.Plugin system is removed as of 2.5.x.

--- play-plugin/src/main/scala/scalikejdbc/async/PlayPlugin.scala
+++ play-plugin/src/main/scala/scalikejdbc/async/PlayPlugin.scala
@@ -1,16 +1,21 @@
 package scalikejdbc.async
 
 import java.nio.charset.Charset
+import javax.inject.Inject
 
-import scalikejdbc._
 import play.api._
+import play.api.inject.ApplicationLifecycle
+import scalikejdbc._
 
+import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 
 /**
  * The Play plugin to use ScalikeJDBC
  */
-class PlayPlugin(implicit app: Application) extends Plugin {
+trait PluginModule
+
+class PlayPlugin @Inject() (implicit app: Application, lifecycle: ApplicationLifecycle) extends PluginModule {
 
   import PlayPlugin._
 
@@ -28,66 +33,70 @@ class PlayPlugin(implicit app: Application) extends Plugin {
 
   private[this] var closeAllOnStop = true
 
-  override def onStart(): Unit = {
-    playDbConfig.subKeys map {
-      name =>
-        def load(name: String): (String, String, String, AsyncConnectionPoolSettings) = {
-          implicit val config = playDbConfig
-          val default = AsyncConnectionPoolSettings()
-          val connectionSettings = AsyncConnectionSettings(
-            charset = opt(name, "charset").map(v => Charset.forName(v)),
-            maximumMessageSize = opt(name, "maximumMessageSize").map(v => v.toInt),
-            connectTimeout = opt(name, "connectTimeout").map(v => Duration(v)),
-            testTimeout = opt(name, "testTimeout").map(v => Duration(v)),
-            queryTimeout = opt(name, "queryTimeout").map(v => Duration(v))
-          )
-          val settings = AsyncConnectionPoolSettings(
-            maxPoolSize = opt(name, "maxPoolSize").map(v => v.toInt).getOrElse(default.maxPoolSize),
-            maxIdleMillis = opt(name, "maxIdleMillis").map(v => v.toLong).getOrElse(default.maxIdleMillis),
-            maxQueueSize = opt(name, "maxQueueSize").map(v => v.toInt).getOrElse(default.maxQueueSize),
-            connectionSettings = connectionSettings
-          )
-          (require(name, "url"), opt(name, "user").getOrElse(""), opt(name, "password").getOrElse(""), settings)
-        }
-
-        registeredPoolNames.synchronized {
-          name match {
-            case "global" =>
-              // because "db.global" was used as "scalikejdbc.global" previously
-              Logger(classOf[PlayPlugin]).warn("Configuration with \"db.global\" is ignored. Use \"scalikejdbc.global\" instead.")
-            case _ =>
-              if (!registeredPoolNames.contains(name)) {
-                val (url, user, password, settings) = load(name)
-                AsyncConnectionPool.add(Symbol(name), url, user, password, settings)
-                registeredPoolNames.add(name)
-              }
-          }
-        }
-    }
-
-    opt(loggingSQLAndTime, "enabled")(globalConfig).map(_.toBoolean).foreach {
-      enabled =>
-        implicit val config = globalConfig
-        val default = LoggingSQLAndTimeSettings()
-        GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
-          enabled = enabled,
-          singleLineMode = opt(loggingSQLAndTime, "singleLineMode").map(_.toBoolean).getOrElse(default.singleLineMode),
-          logLevel = opt(loggingSQLAndTime, "logLevel").map(v => Symbol(v)).getOrElse(default.logLevel),
-          warningEnabled = opt(loggingSQLAndTime, "warningEnabled").map(_.toBoolean).getOrElse(default.warningEnabled),
-          warningThresholdMillis = opt(loggingSQLAndTime, "warningThresholdMillis").map(_.toLong).getOrElse(default.warningThresholdMillis),
-          warningLogLevel = opt(loggingSQLAndTime, "warningLogLevel").map(v => Symbol(v)).getOrElse(default.warningLogLevel)
+  //  override def onStart(): Unit = {
+  playDbConfig.subKeys map {
+    name =>
+      def load(name: String): (String, String, String, AsyncConnectionPoolSettings) = {
+        implicit val config = playDbConfig
+        val default = AsyncConnectionPoolSettings()
+        val connectionSettings = AsyncConnectionSettings(
+          charset = opt(name, "charset").map(v => Charset.forName(v)),
+          maximumMessageSize = opt(name, "maximumMessageSize").map(v => v.toInt),
+          connectTimeout = opt(name, "connectTimeout").map(v => Duration(v)),
+          testTimeout = opt(name, "testTimeout").map(v => Duration(v)),
+          queryTimeout = opt(name, "queryTimeout").map(v => Duration(v))
         )
-    }
+        val settings = AsyncConnectionPoolSettings(
+          maxPoolSize = opt(name, "maxPoolSize").map(v => v.toInt).getOrElse(default.maxPoolSize),
+          maxIdleMillis = opt(name, "maxIdleMillis").map(v => v.toLong).getOrElse(default.maxIdleMillis),
+          maxQueueSize = opt(name, "maxQueueSize").map(v => v.toInt).getOrElse(default.maxQueueSize),
+          connectionSettings = connectionSettings
+        )
+        (require(name, "url"), opt(name, "user").getOrElse(""), opt(name, "password").getOrElse(""), settings)
+      }
 
-    opt("closeAllOnStop", "enabled")(playConfig).foreach { enabled => closeAllOnStop = enabled.toBoolean }
-
+      registeredPoolNames.synchronized {
+        name match {
+          case "global" =>
+            // because "db.global" was used as "scalikejdbc.global" previously
+            Logger(classOf[PlayPlugin]).warn("Configuration with \"db.global\" is ignored. Use \"scalikejdbc.global\" instead.")
+          case _ =>
+            if (!registeredPoolNames.contains(name)) {
+              val (url, user, password, settings) = load(name)
+              AsyncConnectionPool.add(Symbol(name), url, user, password, settings)
+              registeredPoolNames.add(name)
+            }
+        }
+      }
   }
 
-  override def onStop(): Unit = {
-    if (closeAllOnStop) {
-      AsyncConnectionPool.closeAll()
-      registeredPoolNames.synchronized(registeredPoolNames.clear())
-    }
+  opt(loggingSQLAndTime, "enabled")(globalConfig).map(_.toBoolean).foreach {
+    enabled =>
+      implicit val config = globalConfig
+      val default = LoggingSQLAndTimeSettings()
+      GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
+        enabled = enabled,
+        singleLineMode = opt(loggingSQLAndTime, "singleLineMode").map(_.toBoolean).getOrElse(default.singleLineMode),
+        logLevel = opt(loggingSQLAndTime, "logLevel").map(v => Symbol(v)).getOrElse(default.logLevel),
+        warningEnabled = opt(loggingSQLAndTime, "warningEnabled").map(_.toBoolean).getOrElse(default.warningEnabled),
+        warningThresholdMillis = opt(loggingSQLAndTime, "warningThresholdMillis").map(_.toLong).getOrElse(default.warningThresholdMillis),
+        warningLogLevel = opt(loggingSQLAndTime, "warningLogLevel").map(v => Symbol(v)).getOrElse(default.warningLogLevel)
+      )
+  }
+
+  opt("closeAllOnStop", "enabled")(playConfig).foreach { enabled => closeAllOnStop = enabled.toBoolean }
+
+  //  }
+
+  lifecycle.addStopHook {
+    () =>
+      {
+        if (closeAllOnStop) {
+          AsyncConnectionPool.closeAll()
+          registeredPoolNames.synchronized(registeredPoolNames.clear())
+        }
+        Future.successful(())
+      }
   }
 }
 
